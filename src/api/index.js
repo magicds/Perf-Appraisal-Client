d4b2f2857a616562e107e3aea3f92eb98aea6975
cdswyda <cdswyda@gmail.com>
2018-08-24 17:44:22 +0800

    update

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,15 @@ import config from './config';
 
 const path = `${config.protocol}://${config.host}${config.prefix}`;
 
+axios.interceptors.response.use((response) => {
+  // console.log(response);
+  return response.data;
+}, (err) => {
+  console.log(err);
+  return err.response;
+});
+
 export default axios.create({
-  baseURL: path
+  baseURL: path,
+  withCredentials: true
 });
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,14 +1,24 @@
 <template>
   <div>
-    <el-input v-model="email" type="text" placeholder="请输入邮箱"></el-input>
-    <el-input v-model="pwd" type="password" placeholder="请输入密码"></el-input>
-    <el-button @click="login" :loading="loading">登录</el-button>
+    <el-form :model="user" ref="form" label-position="right" label-width="80px" :rules="rules">
+      <el-form-item prop="email"  label="邮箱">
+        <el-input v-model="user.email" type="text" placeholder="请输入邮箱"></el-input>
+      </el-form-item>
+      <el-form-item prop="pwd" label="密码">
+        <el-input  v-model="user.pwd" type="password" placeholder="请输入密码"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="handleLogin" :loading="loading">登录</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
-import { Input, Button, Form, FormItem } from 'element-ui';
+import { Input, Button, Form, FormItem, Message } from 'element-ui';
 import api from '../api/';
+
+window.api = api;
 export default {
   name: 'login',
   components: {
@@ -19,31 +29,46 @@ export default {
   },
   data() {
     return {
-      email: '',
-      pwd: '',
+      user: {
+        email: '',
+        pwd: ''
+      },
+      rules: {
+        email: [{ required: true, message: '请输入邮箱', trigger: 'blur' }, { type: 'email', message: '邮箱格式不正确', trigger: 'blur' }],
+        pwd: [{ required: true, message: '请输入密码', trigger: 'blur' }]
+      },
       loading: false
     };
   },
   computed: {
     encryptedPwd() {
-      return btoa(this.pwd);
+      return btoa(this.user.pwd);
     }
   },
   methods: {
-    login() {
-      if (!this.pwd || !this.email) {
-        return;
-      }
-      this.loading = true;
-      api
-        .post('/user/login', {
-          email: this.email,
-          pwd: this.encryptedPwd
-        })
-        .then(res => {
-          console.log(res.data);
-          this.loading = false;
-        });
+    handleLogin() {
+      this.$refs.form.validate().then(isValidate => {
+        if (!isValidate) return;
+        this.loading = true;
+        api
+          .post('/user/login', {
+            email: this.user.email,
+            pwd: this.encryptedPwd
+          })
+          .then(res => {
+            console.log(res.data);
+            this.loading = false;
+          })
+          .catch(err => {
+            console.log(err.response);
+            this.loading = false;
+            const data = err.response.data;
+            Message({
+              message: `${data.code || ''} ${data.msg || data.message}`,
+              type: 'error'
+            });
+          });
+      });
     }
   }
 };
